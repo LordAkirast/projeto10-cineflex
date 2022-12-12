d70b2643778b1dce7509a52b53f861a351d16ffd
LordAkirast <darkwarish@gmail.com>
2022-12-12 17:27:58 -0300

    Undone

--- projeto10-cineflex/src/App.js
+++ projeto10-cineflex/src/App.js
@@ -3,19 +3,26 @@ import { SelectMovie } from "./styled"
 import axios from 'axios';
 import { useState } from "react";
 import { MoviesImg } from "./styled";
+import { useEffect } from "react";
 
 
 
 export default function App() {
     const [releaseDate, setreleaseDate] = useState('a')
     const [posterURL, setposterURL] = useState('b')
+    const [title, settitle] = useState('b')
+    const [id, setid] = useState('b')
+    const [overview, setoverview] = useState('b')
+    const [ApiThings,setApiThings] = useState([]);
+
+    useEffect(() => {getMovies()}, [])
 
     return (
         <div>
         <Cineflex>CINEFLEX</Cineflex>
         <SelectMovie>Selecione o filme</SelectMovie>
         <div onClick={getMovies}>
-            <img style={{width: 130, height: 195}} src={posterURL} alt={releaseDate}/>
+            {ApiThings.map(()  => <img key={title} style={{width: 130, height: 195}} src={posterURL} alt={releaseDate}/>)}
             
         </div>
         </div>
@@ -25,13 +32,16 @@ export default function App() {
 function getMovies() {
     const promise =  axios.get('https://mock-api.driven.com.br/api/v8/cineflex/movies')
      .then((response) => {
-        console.log(response.data[0].posterURL)
-         for(let i in response.data) {
-         let id = response.data[i].id
-         let title = response.data[i].title
-         setposterURL(response.data[0].posterURL)
-         let overview = response.data[i].overview
+        let i = 0;
+         while( i < response.data.length) {
+         setid(response.data[i].id)
+         settitle(response.data[i].title)
+         setposterURL(response.data[i].posterURL)
+         setoverview(response.data[i].overview)
          setreleaseDate(response.data[i].releaseDate)
+         setApiThings([response.data[i].id,response.data[i].title,response.data[i].posterURL,response.data[i].overview,response.data[i].releaseDate] )
+         console.log(ApiThings)
+         i++;
          }
      })
  }
